a74c21877cb4fc8820fdd35012d0ba39659623b9
vaspatrik <vas.patrik.d@gmmail.com>
2015-05-05 01:08:16 -0400

    Add socket connection parameters

--- Server/GlobalClass.cpp
+++ Server/GlobalClass.cpp
@@ -2,7 +2,22 @@
 
 
 const std::string GlobalClass::DATABASE_HOST = "tcp://127.0.0.1:3306";
-const std::string GlobalClass::DATABASE_NAME = "CHAT_DB";
+const std::string GlobalClass::DATABASE_NAME = "chat_db";
 
-const std::string GlobalClass::DATABASE_USER = "DBUSER";
+const std::string GlobalClass::DATABASE_USER = "dbuser";
 const std::string GlobalClass::DATABASE_PASSWORD = "dbpassword";
+
+
+
+const int GlobalClass::PORT_NUMBER = 9032;
+
+
+std::string GlobalClass::converter(int a) {
+	std::stringstream buffer;
+	std::string strin;
+	buffer << a;
+	buffer >> strin;
+	buffer.str("");
+	buffer.clear();
+	return strin;
+}
--- Server/GlobalClass.h
+++ Server/GlobalClass.h
@@ -2,13 +2,21 @@
 #define GLOBALCLASS_H
 
 #include <string>
+#include <ctime>
+#include <sstream>
 
 class GlobalClass {
 public:
+
+
 	static const std::string DATABASE_HOST;
 	static const std::string DATABASE_NAME;
 	static const std::string DATABASE_USER;
 	static const std::string DATABASE_PASSWORD;
+
+	static const int PORT_NUMBER;
+
+	static std::string converter(int a); 
 };
 
 
