8a66bffc5babd5412979cdf27a48163393a58529
Blenesi Attila <ablenesi@gmail.com>
2015-05-16 05:23:04 +0200

    Update Client MesaggeBorad KeyListener

--- Client/src/main/java/controller/MessageBoardController.java
+++ Client/src/main/java/controller/MessageBoardController.java
@@ -2,23 +2,22 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
+import javax.swing.JTextArea;
 
 import model.Message;
 import view.MessageBoard;
 
-public class MessageBoardController implements ActionListener{
+public class MessageBoardController implements ActionListener, KeyListener{
 	private Controller controller;
-	
+
 	public MessageBoardController(Controller controller) {
 		this.controller = controller;
 	}
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		MessageBoard messageBoard =(MessageBoard)((JButton)e.getSource()).getParent();
-		//System.out.println("Send this message: "+messageBoard.getMessage()+" to: "+messageBoard.getUser().getUserName());
+	private void sendMessage(MessageBoard messageBoard) {
 		String mess = messageBoard.getMessage();
 		if(!mess.equals("")){
 			System.out.println("in controller "+mess);
@@ -28,5 +27,26 @@ public class MessageBoardController implements ActionListener{
 			controller.getServerHandler().sendMessage(messageToServet);
 		}
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		MessageBoard messageBoard =(MessageBoard)((JButton)e.getSource()).getParent();		
+		sendMessage(messageBoard);
+	}
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_ENTER){
+			MessageBoard messageBoard = (MessageBoard)((JTextArea)e.getSource()).getParent();		
+			sendMessage(messageBoard);
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) { }
+
+	@Override
+	public void keyTyped(KeyEvent e) { }
 
 }
